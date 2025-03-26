4be0c549f5543640674cc00d1666afb63e6f205a
soonstal <agentblue6@yahoo.com>
2025-03-26 22:36:55 +0000

    Update API - fixing typo

--- src/utils/use-async.spec.ts
+++ src/utils/use-async.spec.ts
@@ -3,20 +3,20 @@ import { describe, expect, it, vi } from 'vitest'
 import useAsync from 'src/utils/use-async'
 
 describe('# Create async process', () => {
-  const someProcess = (): Promise<null> => Promise.resolve(null)
+  let someProcess = (): Promise<null> => Promise.resolve(null)
 
   it('should expect active as Vue Ref type', () => {
-    const { active } = useAsync(someProcess)
+    let { active } = useAsync(someProcess)
 
     expect(isRef(active)).toBe(true)
   })
 
   it('should correctly test active functionality', async () => {
-    const { active, run } = useAsync(someProcess)
+    let { active, run } = useAsync(someProcess)
 
     expect(active.value).toBe(false)
 
-    const promise = run()
+    let promise = run()
 
     expect(active.value).toBe(true)
 
@@ -26,16 +26,16 @@ describe('# Create async process', () => {
   })
 
   it('should expect run as a function', () => {
-    const { run } = useAsync(someProcess)
+    let { run } = useAsync(someProcess)
 
     expect(run).toBeInstanceOf(Function)
   })
 
   it('should expect original function called with correct params and return correct data', async () => {
-    const someProcess = vi.fn().mockResolvedValue({ a: 1, b: null })
-    const { run } = useAsync(someProcess)
+    let someProcess = vi.fn().mockResolvedValue({ a: 1, b: null })
+    let { run } = useAsync(someProcess)
 
-    const result = await run(null)
+    let result = await run(null)
     expect(result).toEqual({ a: 1, b: null })
     expect(someProcess).toBeCalledWith(null)
   })
